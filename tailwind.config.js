463e3e80a06190887cb36d5eb179af831d10e7e5
Moh Iqbal Kharisma S <147675426+iqbalkharisma@users.noreply.github.com>
2026-06-08 02:10:57 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        rosegold: '#B76E79',
+        amethyst: '#9966CC',
+        champagne: '#F7E7CE',
+        dark: '#121212',
+      },
+      fontFamily: {
+        serif: ['Playfair Display', 'serif'],
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
